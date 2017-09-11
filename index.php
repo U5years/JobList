11fee14436e2c5f70b0be40a3a9b17016181b10b
Tao <tjiang6@stevens.edu>
2017-09-11 13:06:31 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include_once 'conf/init.php'; ?>
+
+<?php
+$template = new Template('templates/frontpage.php');
+
+
+
+echo $template;
+
---
+++ templates/frontpage.php
@@ -0,0 +1,41 @@
+<?php include 'inc/header.php'; ?>
+	<div class="jumbotron">
+        <h1>Heading</h1>
+            <select name="category" class="form-control">
+                <option value="0">Choose Category</option>
+            </select>
+            <br>
+            <input type="submit" class="btn btn-lg btn-success" value="FIND">
+        </form>
+      </div>
+  	<div class="row marketing">
+        <div class="col-md-10">
+        	<h4>Subheading</h4>	
+          	<p>description</p>     
+        </div>
+        <div class="col-md-2">
+        		<a class="btn btn-default" href="#">View</a>
+        </div>
+    </div>
+    
+    <div class="row marketing">
+        <div class="col-md-10">
+        	<h4>Subheading</h4>	
+          	<p>description</p>     
+        </div>
+        <div class="col-md-2">
+        		<a class="btn btn-default" href="#">View</a>
+        </div>
+    </div>
+
+    <div class="row marketing">
+        <div class="col-md-10">
+        	<h4>Subheading</h4>	
+          	<p>description</p>     
+        </div>
+        <div class="col-md-2">
+        		<a class="btn btn-default" href="#">View</a>
+        </div>
+    </div>
+         
+<?php include 'inc/footer.php'; ?>
