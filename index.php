adbd6006180fa6d4274a73481e4335ccf254b250
Tao <tjiang6@stevens.edu>
2017-09-11 10:37:56 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
-<?php include_once 'config/init.php'; ?>
+<?php include_once 'conf/init.php'; ?>
 
 <?php
-$job = new Job;
-
 $template = new Template('templates/frontpage.php');
 
-$category = isset($_GET['category']) ? $_GET['category'] : null;
-
-if($category){
-		$template->jobs = $job->getByCategory($category);
-		$template->title = 'Jobs In '. $job->getCategory($category)->name;
-} else {
-		$template->title = 'Latest Jobs';
-		$template->jobs = $job->getAllJobs();
-}
-
-$template->categories = $job->getCategories();
-
 echo $template;
+
---
+++ templates/inc/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Job Lister</title>
+	<link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">Create Listing</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">JobLister</h3>
+     </div>
+
