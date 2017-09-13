60b7f57c7dd61693b77f8fdca9244fe43bf034c6
Jiahuang Zhou <jzhou23@stevens.edu>
2017-09-13 02:27:03 +0000

    update newJob form

--- templates/new.php
+++ templates/new.php
@@ -16,11 +16,53 @@
                 <label for="description">Description</label>
                 <textarea id="description" class="form-control" name="description" rows="5"></textarea>
             </div>
+            
+            <div class="row">
+                <div class="form-check col-sm-3 col-xs-6">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" name="tag" value="font-end">
+                            font-end
+                    </label>
+                </div>
+                <div class="form-check col-sm-3 col-xs-6">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" name="tag" value="back-end">
+                            back-end
+                     </label>
+                </div>
+                <div class="form-check col-sm-3 col-xs-6">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" name="tag" value="full-stack">
+                            full-stack
+                     </label>
+                </div>
+                <div class="form-check col-sm-3 col-xs-6">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" name="tag" value="data science">
+                            data science
+                     </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-check col-sm-3 col-xs-6">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" name="tag" value="mobile developement">
+                            mobile developement
+                     </label>
+                </div>
+                <div class="form-check col-sm-3 col-xs-6">
+                    <label class="form-check-label">
+                        <input class="form-check-input" type="checkbox" name="tag" value="other">
+                            other
+                     </label>
+                </div>
+            </div>
             <div class="form-group"/>
-                <button class="btn btn-lg btn-primary btn-block">Submit</button>
+                <button class="btn btn-md btn-primary btn-block">Submit</button>
             </div>
         </form>
         <a href="#">Go back</a>
     </div>
 
 <?php include 'inc/footer.php'; ?>
+
