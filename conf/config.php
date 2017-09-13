3a7f7f34e39340d1dca421cd27b6b2e7743637e0
Tao <tjiang6@stevens.edu>
2017-09-13 12:27:35 -0700

    Add files via upload

--- conf/config.php
+++ conf/config.php
@@ -1,2 +1,8 @@
-<!-- <?php
-	echo 'hello world'; -->
+<?php
+// DB Params
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "jzhou23");
+define("DB_PASS", "");
+define("DB_NAME", "joblister");
+
+define("SITE_TITLE", "JobLister");
