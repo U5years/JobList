68c300f02aa3ffa5f398c835360a640f464e4fe8
Shenwei Chen <chenshenwei92104@gmail.com>
2017-09-14 01:56:05 -0400

    Update header.php

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -10,8 +10,8 @@
       <div class="header clearfix">
         <nav>
           <ul class="nav nav-pills pull-right">
-            <li role="presentation"><a href="login.php">login</a></li>
-            <li role="presentation"><a href="templates/new.php">Create New</a></li>
+            <li role="presentation"><a href="../login.php">login</a></li>
+            <li role="presentation"><a href="../new.php">Create New</a></li>
           </ul>
         </nav>
         <h3 class="text-muted">JobLister</h3>
